2624ff4d48d2a237b239117e70fff0dd5c3762bb
brunovongxay <vongxay.bruno@gmail.com>
2019-10-07 19:23:20 +0200

    Ajout 04_simon_says.rb

--- lib/04_simon_says.rb
+++ lib/04_simon_says.rb
@@ -14,8 +14,17 @@ def repeat(string, n = n.to_i)
     end 
 end
 
-def start_of_word(string)
-    string.split[0]
+def start_of_word(string,n)
+    return string[0,n]
 end
 
+def first_word(string)
+    return string.split(' ')[0]
+end
 
+def titleize(string)
+   little_words = %w(end and the)
+   string.capitalize.gsub(/(\w+)/) do |word|
+     little_words.include?(word) ? word : word.capitalize
+   end
+end
